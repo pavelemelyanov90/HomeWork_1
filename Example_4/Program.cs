c2c340cd2273c36a7657b865a36be7da6d90f334
Pavel <kwadrakilla666@gmail.com>
2023-01-16 16:55:44 +0300

    add Example_4

---
+++ Example_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4 : чётные числа от 1го до N
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+
+while(count <= number)
+{
+    if(count % 2 == 0)
+    {
+        Console.WriteLine($" {count} ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}

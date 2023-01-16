df1ce3979397c7e1ee472408e4703f2eee0ce3b5
Pavel <kwadrakilla666@gmail.com>
2023-01-16 16:35:42 +0300

    add Example_3

---
+++ Example_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3 : определение чётности введённого числа
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if(number % 2 == 0) // % показывает остаток от деления 
+{
+    Console.WriteLine($"Число {number} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечётное");
+}

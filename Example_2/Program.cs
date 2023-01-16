bbbaa861ccb86da9455715628d5a10db7f26c87b
Pavel <kwadrakilla666@gmail.com>
2023-01-16 16:26:38 +0300

    add Example_2

---
+++ Example_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2 : наибольшее из 3х чисел
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int number3 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+
+if(number1 > max)
+{
+    max = number1;
+}
+if(number2 > max)
+{
+    max = number2;
+}   
+if(number3 > max)
+{
+    max = number3;
+} 
+
+Console.WriteLine($"Максимальное число {max}");
+
+

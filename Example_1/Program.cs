3c299a99f34d68452fdf247e56264634ffe23978
Pavel <kwadrakilla666@gmail.com>
2023-01-16 16:00:48 +0300

    add Example_1

---
+++ Example_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: нахождение максимального из 2х чисел
+
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Максимальное число {number1}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {number2}");
+}        
